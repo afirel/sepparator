1a5af3a52f137a0b993e129e3b67df97dac1f128
Andreas Brandl <abrandl@crealytics.de>
2013-07-16 16:13:22 +0200

    Add module method to convert CSVs to Excel.

--- lib/sepparator.rb
+++ lib/sepparator.rb
@@ -7,4 +7,9 @@ module Sepparator
     Console.start(args)
   end
 
+  def csv_to_excel(csv_path, excel_path, col_sep: ';', sheet_name: 'from CSV')
+    converter = SpreadsheetConverter.new(col_sep: col_sep)
+    converter.convert(csv_path, excel_path, sheet_name: sheet_name)
+  end
+
 end
---
+++ spec/sepparator_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Sepparator do
+
+  context '.csv_to_excel' do
+    it 'creates a converter and uses it appropriately' do
+      col_sep = ','
+      csv_path = 'foo/bar.csv'
+      xls_path = 'bla/fu.xls'
+      sheet_name = 'my sheet'
+      converter = stub
+      Sepparator::SpreadsheetConverter.should_receive(:new).with(col_sep: col_sep).and_return(converter)
+      converter.should_receive(:convert).with(csv_path, xls_path, sheet_name: sheet_name)
+
+      Sepparator.csv_to_excel(csv_path, xls_path, sheet_name: sheet_name, col_sep: col_sep)
+    end
+
+  end
+
+end
+
